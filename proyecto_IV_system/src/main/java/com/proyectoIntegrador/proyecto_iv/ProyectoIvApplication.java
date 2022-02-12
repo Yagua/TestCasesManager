33653764a748b6f464c508314893b4675897bd0f
Yagua <yagua.db@gmail.com>
2022-02-12 17:30:57 -0500

    update project structure

--- proyecto_IV_system/src/main/java/com/proyectoIntegrador/proyecto_iv/ProyectoIvApplication.java
+++ proyecto_IV_system/src/main/java/com/proyectoIntegrador/proyecto_iv/ProyectoIvApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoIvApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProyectoIvApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoIvApplication.class, args);
+    }
 }
