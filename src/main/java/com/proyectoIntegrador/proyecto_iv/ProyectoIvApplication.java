f86a43432e3622ab37a0efb9c709358899a758a1
Yagua <yagua.db@gmail.com>
2022-02-11 20:47:23 -0500

    create project

---
+++ src/main/java/com/proyectoIntegrador/proyecto_iv/ProyectoIvApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoIntegrador.proyecto_iv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoIntegrador/proyecto_iv/ProyectoIvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoIntegrador.proyecto_iv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
